36aa300ccb2da4e935df5c78867db4b503befa87
Jessie Keck <jessie.keck@gmail.com>
2012-11-12 09:34:44 -0800

    Add basic rspec support.

--- Gemfile
+++ Gemfile
@@ -21,6 +21,11 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+group :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+end
+
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
---
+++ spec/models/solr_document_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe SolrDocument do
+  it "should do something" do
+    doc = SolrDocument.new(:id => "12345")
+    doc.should be_a SolrDocument
+    doc[:id].should == "12345"
+    doc.should respond_to :export_formats
+  end
+end
