b3922eacd4c651a87344278b4e463f73f941f5f1
Gary Geisler <ggeisler@gmail.com>
2012-11-11 18:07:59 -0800

    Add custom layout file to enable also adding Stanford footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class ApplicationController < ActionController::Base
   # Please be sure to impelement current_user and user_session. Blacklight depends on 
   # these methods in order to perform user specific actions. 
 
+  def layout_name
+    "revs"
+  end
+      
   protect_from_forgery
 end
