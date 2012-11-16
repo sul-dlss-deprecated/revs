2f42adda024507a16975541b58d877b33a094ab2
Jessie Keck <jessie.keck@gmail.com>
2012-11-16 15:01:14 -0800

    Add carousel of all collections to home page.

--- app/assets/javascripts/revs-carousel.js
+++ app/assets/javascripts/revs-carousel.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+	// Carousel on Collection show page
   $("#collection_carousel").bind('slid', function(){
 	  var carousel = $(this);
-	  var index = $('.active', $(this)).index('#' + $(this).attr("id") + ' .item');
-	  $(".iterator", $(this)).text(parseInt(index) + 1);
+	  var index = $('.active', carousel).index('#' + carousel.attr("id") + ' .item');
+	  $(".iterator", carousel).text(parseInt(index) + 1);
 	});	
+	
+	// Carousel on home page
+	$("#all_collections_carousel").bind('slid', function(){
+		var active_item = $(".active", $(this));
+		$(".featured-description #collection-title").text(active_item.attr("data-collection-title"));
+		$(".featured-description #collection-description").text(active_item.attr("data-collection-description"));
+		$(".featured-description #collection-link").attr("href", active_item.attr("data-collection-href"));
+	});
 });
