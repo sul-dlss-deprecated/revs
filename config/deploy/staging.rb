3d23e429defaaf3cc172d03930d92f35e275b484
Jessie Keck <jessie.keck@gmail.com>
2012-11-21 13:10:50 -0800

    Add jetty changes to staging deploy script.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,4 +8,8 @@ role :web, deployment_host
 role :app, deployment_host
 role :db,  deployment_host, :primary => true
 
+before "deploy", "jetty:stop"
 before "deploy:migrate", "db:symlink_sqlite"
+after "deploy", "jetty:start"
+after "deploy", "db:loadfixtures"
+after "jetty:start", "revs:index_fixtures"
