4540c2a17da0a03cac8be03198a87b4daf80082f
Jessie Keck <jessie.keck@gmail.com>
2012-11-21 12:02:42 -0800

    Add basic modal popup support for jQuery zoom/pan plugin.

---
+++ app/assets/javascripts/revs.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	// Modal behavior for collection member show page.
+	$("[data-modal-selector]").on('click', function(){
+		$($(this).attr("data-modal-selector")).modal('show');
+	  return false;
+	});
+		
+});
