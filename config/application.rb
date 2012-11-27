9924697485b0d7d9e4537305dbc2e8cdfa7016ea
Gary Geisler <ggeisler@gmail.com>
2012-11-27 11:25:13 -0800

    Update layout of Contact Us page

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,5 @@ module Revs
 end
 
 Revs::Application.config.stacks_url = YAML.load_file("#{Rails.root}/config/stacks.yml")[Rails.env]["url"]
-Revs::Application.config.contact_us_topics = {'terms of use'=>'Terms of Use Question', 'error'=>'Problem with the website','other'=>'Other questions'} # sets the list of topics shown in the contact us page
-Revs::Application.config.contact_us_recipients = {'error'=>'petucket@stanford.edu','terms of use'=>'petucket@stanford.edu','other'=>'petucket@stanford.edu'} # sets the email address for each contact us topic configed above
+Revs::Application.config.contact_us_topics = {'default'=>'Select a topic...', 'terms of use'=>'Terms of Use question', 'error'=>'Problem with the website','other'=>'Other questions'} # sets the list of topics shown in the contact us page
+Revs::Application.config.contact_us_recipients = {'default'=>'petucket@stanford.edu', 'error'=>'petucket@stanford.edu','terms of use'=>'petucket@stanford.edu','other'=>'petucket@stanford.edu'} # sets the email address for each contact us topic configed above
