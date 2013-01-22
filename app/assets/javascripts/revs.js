c93b165dafc230da3bf7544c0bb365a013663815
Peter Mangiafico <peter@mangiafico.org>
2013-01-22 10:25:34 -0800

    fix for REV-256, home page with JS disabled

--- app/assets/javascripts/revs.js
+++ app/assets/javascripts/revs.js
@@ -5,5 +5,10 @@ $(document).ready(function(){
 		$($(this).attr("data-modal-selector")).modal('show');
 	  return false;
 	});
+	
+	// elements defined with these classes can be hidden by default and then show when the page loads
+	//  useful when you have non javascript friendly DOM elements you need to hide for no JS browsers so you can include a <noscript> tag with
+	//   non JS versions
+	$(".showOnLoad").show();
 		
 });
