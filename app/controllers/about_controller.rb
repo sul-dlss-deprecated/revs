7481f7dd64cfdb0b634c2c0f38be4b27c3d9b4c7
Peter Mangiafico <peter@mangiafico.org>
2013-03-28 15:33:43 -0700

    add a feedback link at the bottom of the item detail page; add a new topic for metadata feedback

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,15 +4,26 @@ class AboutController < ApplicationController
   # If your action has logic that needs to be run before the view, create a method, call "show" at the end of it, create a view partical o match,
   # and add a custom route in the routes.rb file    
   def contact
+    
     @from=params[:from]
+    @subject=params[:subject]
+    @name=params[:name]
+    @email=params[:email]
+    @message=params[:message]
+
     if request.post?
-      @subject=params[:subject]
-      @name=params[:name]
-      @email=params[:email]
-      @message=params[:message]
       unless @message.blank?
         RevsMailer.contact_message(:params=>params,:request=>request).deliver 
-        flash[:notice]=t("revs.about.contact_message_sent")
+        
+        if @subject=='metadata'
+          flash[:notice]=t("revs.about.contact_message_sent_about_metadata")
+        else
+          flash[:notice]=t("revs.about.contact_message_sent")          
+        end
+        
+        @message=nil
+        @name=nil
+        @email=nil
         unless @from.blank?
           redirect_to(@from)
           return
@@ -22,6 +33,7 @@ class AboutController < ApplicationController
       end
     end
     show
+    
   end
 
   def show
