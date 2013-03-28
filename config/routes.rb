1905514dff72a23c9141c52a158e375c56187783
Peter Mangiafico <peter@mangiafico.org>
2013-03-28 15:37:37 -0700

    add a friendlier route for contact us

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Revs::Application.routes.draw do
   
   # Handles all About pages.
   match 'about', :to => 'about#show', :as => 'about_project', :defaults => {:id=>'project'} # no page specified, go to project page
-  match 'about/contact', :to=> 'about#contact', :as=>'contact_us' # specific contact us about page
+
+  match 'contact', :to=> 'about#contact', :as=>'contact_us'
+  match 'about/contact', :to=> 'about#contact' # specific contact us about page
   match 'about/:id', :to => 'about#show' # catch anything else and direct to show page with ID parameter of partial to show
   
   # helper routes to we can have a friendly URL for items and collections
