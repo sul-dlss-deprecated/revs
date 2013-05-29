63bbe814f9d65a85e0720288d248a2e164aec507
Gary Geisler <ggeisler@gmail.com>
2013-05-29 15:39:25 -0700

    Add methods to solr_document model for new item metadata fields

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-class SolrDocument 
+class SolrDocument
 
   include Blacklight::Solr::Document
 
@@ -8,45 +8,109 @@ class SolrDocument
   def flags
     Flag.includes(:user).where(:druid=>id)
   end
-  
+
   def annotations
     Annotation.includes(:user).where(:druid=>id)
   end
-  
+
   def title
     self[:title_tsi]
   end
-  
+
   def description
     self[blacklight_config.collection_description_field.to_sym]
   end
-  
+
   def years
     self[:pub_year_isim]
   end
-  
+
+  def people
+    self[:people_ssim]
+  end
+
+  def subjects
+    self[:subjects_ssim]
+  end
+
+  def location
+    self[:location_ssi]
+  end
+
   def formats
     self[:format_ssim]
   end
-  
+
   def identifier
     self[:source_id_ssi]
   end
-  
+
+  def institutional_notes
+    self[:inst_notes_tsi]
+  end
+
+  def production_notes
+    self[:prod_notes_tsi]
+  end
+
+  def metadata_sources
+    self[:metadata_sources_tsi]
+  end
+
+  def has_more_metadata
+    self[:has_more_metadata_ssi]
+  end
+
+  def vehicle_markings
+    self[:vehicle_markings_tsi]
+  end
+
+  def marque
+    self[:marque_ssim]
+  end
+
+  def current_owner
+    self[:current_owner_ssi]
+  end
+
+  def entrant
+    self[:entrant_ssi]
+  end
+
+  def venue
+    self[:venue_ssi]
+  end
+
+  def track
+    self[:track_ssi]
+  end
+
+  def event
+    self[:event_ssi]
+  end
+
+  def group_class
+    self[:group_class_tsi]
+  end
+
+  def race_data
+    self[:race_data_tsi]
+  end
+
   def collection_names
     self[blacklight_config.collection_member_collection_title_field.to_sym]
   end
 
   def collection?
-    self.has_key?(blacklight_config.collection_identifying_field) and 
+    self.has_key?(blacklight_config.collection_identifying_field) and
       self[blacklight_config.collection_identifying_field].include?(blacklight_config.collection_identifying_value)
   end
-  
+
   def collection_member?
-    self.has_key?(blacklight_config.collection_member_identifying_field) and 
+    self.has_key?(blacklight_config.collection_member_identifying_field) and
       !self[blacklight_config.collection_member_identifying_field].blank?
   end
-  
+
   # Return a SolrDocument object of the parent collection of a collection member
   def collection
     return nil unless collection_member?
@@ -58,18 +122,18 @@ class SolrDocument
                       )["response"]["docs"].first
                     )
   end
-  
+
   # Return a CollectionMembers object of just the members of a collection, and cache the result in the object so we can use on the page over again
   def collection_members(params={})
     return nil unless collection?
     @collection_members ||= get_members(params)
   end
-  
+
   # this can be called when you don't want the result to be cached in the object (so you update the counts or start for paging)
   def get_members(params={})
 
     rows=params[:rows] || blacklight_config.collection_member_grid_items
-    start=params[:start] || 0    
+    start=params[:start] || 0
     return CollectionMembers.new(
                               Blacklight.solr.select(
                                 :params => {
@@ -89,14 +153,14 @@ class SolrDocument
     @collection_siblings ||= CollectionMembers.new(
                                Blacklight.solr.select(
                                  :params => {
-                                   :fq => "#{blacklight_config.collection_member_identifying_field}:\"#{self[blacklight_config.collection_member_identifying_field].first}\"", 
+                                   :fq => "#{blacklight_config.collection_member_identifying_field}:\"#{self[blacklight_config.collection_member_identifying_field].first}\"",
                                    :rows => rows.to_s,
-                                  :start => start.to_s                                   
+                                  :start => start.to_s
                                  }
                                )
                              )
   end
-  
+
   # Return an Array of collection SolrDocuments
   def all_collections
     @all_collections ||= Blacklight.solr.select(
@@ -108,7 +172,7 @@ class SolrDocument
       SolrDocument.new(document)
     end
   end
-  
+
   def images(size=:default)
     return nil unless self.has_key?(blacklight_config.image_identifier_field)
     stacks_url = Revs::Application.config.stacks_url
@@ -116,17 +180,17 @@ class SolrDocument
       "#{stacks_url}/#{self["id"]}/#{image_id}#{SolrDocument.image_dimensions[size]}"
     end
   end
-  
+
   # The following shows how to setup this blacklight document to display marc documents
   extension_parameters[:marc_source_field] = :marc_display
   extension_parameters[:marc_format_type] = :marcxml
   use_extension( Blacklight::Solr::Document::Marc) do |document|
     document.key?( :marc_display  )
   end
-  
+
   # Email uses the semantic field mappings below to generate the body of an email.
   SolrDocument.use_extension( Blacklight::Solr::Document::Email )
-  
+
   # SMS uses the semantic field mappings below to generate the body of an SMS email.
   SolrDocument.use_extension( Blacklight::Solr::Document::Sms )
 
@@ -135,22 +199,22 @@ class SolrDocument
   # single valued. See Blacklight::Solr::Document::ExtendableClassMethods#field_semantics
   # and Blacklight::Solr::Document#to_semantic_values
   # Recommendation: Use field names from Dublin Core
-  use_extension( Blacklight::Solr::Document::DublinCore)    
-  field_semantics.merge!(    
+  use_extension( Blacklight::Solr::Document::DublinCore)
+  field_semantics.merge!(
                          :title => "title_tsi",
                          :author => "author_display",
                          :language => "language_facet",
                          :format => "format_ssim"
                          )
-     
+
 
   def self.image_dimensions
     options = {:default => "_square",
                :large   => "_thumb" }
   end
-                         
+
   private
-  
+
   def blacklight_config
     CatalogController.blacklight_config
   end
