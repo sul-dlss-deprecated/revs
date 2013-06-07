1d4a7b6f87c0ecfc8199033d21c3110f354d8a4f
Peter Mangiafico <peter@mangiafico.org>
2013-06-07 09:50:29 -0700

    update to latest version of Rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130219212006) do
+ActiveRecord::Schema.define(:version => 20130513171118) do
+
+  create_table "annotations", :force => true do |t|
+    t.integer  "user_id"
+    t.text     "json"
+    t.string   "text"
+    t.string   "druid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "annotations", ["druid"], :name => "index_annotations_on_druid"
+  add_index "annotations", ["user_id"], :name => "index_annotations_on_user_id"
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -29,6 +41,20 @@ ActiveRecord::Schema.define(:version => 20130219212006) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "flags", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "druid",                           :null => false
+    t.string   "flag_type",  :default => "error", :null => false
+    t.text     "comment"
+    t.datetime "cleared"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
+
+  add_index "flags", ["druid"], :name => "index_flags_on_druid"
+  add_index "flags", ["flag_type"], :name => "index_flags_on_flag_type"
+  add_index "flags", ["user_id"], :name => "index_flags_on_user_id"
+
   create_table "searches", :force => true do |t|
     t.text     "query_params"
     t.integer  "user_id"
@@ -40,8 +66,41 @@ ActiveRecord::Schema.define(:version => 20130219212006) do
   add_index "searches", ["user_id"], :name => "index_searches_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "sunet",                  :default => "",    :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        :default => 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "authentication_token"
+    t.string   "role",                   :default => "",    :null => false
+    t.text     "bio",                    :default => "",    :null => false
+    t.string   "first_name",             :default => "",    :null => false
+    t.string   "last_name",              :default => "",    :null => false
+    t.boolean  "public",                 :default => false, :null => false
+    t.string   "url",                    :default => ""
+    t.string   "username",               :default => "",    :null => false
   end
+
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["sunet"], :name => "index_users_on_sunet"
+  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username"
 
 end
