a0f14df2827f5bb254f041d9892271950675097b
Peter Mangiafico <peter@mangiafico.org>
2014-05-15 12:54:41 -0700

    fix for REV-622; refactoring of user avatars when none are available

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,6 +5,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+  def default_url
+    "default-avatar.png"
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
