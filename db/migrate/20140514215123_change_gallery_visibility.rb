4b76d6592ca787b7b857bfebbfa4ce11ad3e8dd6
Peter Mangiafico <peter@mangiafico.org>
2014-05-16 11:46:42 -0700

    fix gallery migration

--- db/migrate/20140514215123_change_gallery_visibility.rb
+++ db/migrate/20140514215123_change_gallery_visibility.rb
@@ -2,7 +2,7 @@ class ChangeGalleryVisibility < ActiveRecord::Migration
   def change
   	 add_column :galleries, :visibility, :string
   	 Gallery.where(:public=>true).each {|gallery| gallery.update_attribute(:visibility,'public') }
-  	 Gallery.where(:public=>false).each {|gallery| gallery.update_attribute(:visibility,'public') }
+  	 Gallery.where(:public=>false).each {|gallery| gallery.update_attribute(:visibility,'private') }
   	 remove_column :galleries, :public
   end
 end
