d2f2c70283e51a35db351a2ee0e6755ab5a51b1c
Peter Mangiafico <peter@mangiafico.org>
2014-05-16 11:03:26 -0700

    allow admins to view private user profiles

--- db/migrate/20140514215123_change_gallery_visibility.rb
+++ db/migrate/20140514215123_change_gallery_visibility.rb
@@ -1,8 +1,8 @@
 class ChangeGalleryVisibility < ActiveRecord::Migration
   def change
   	 add_column :galleries, :visibility, :string
-  	 Gallery.where(:public=>true).each {|gallery| gallery.update_attribute(:visibility,:public) }
-  	 Gallery.where(:public=>false).each {|gallery| gallery.update_attribute(:visibility,:public) }
+  	 Gallery.where(:public=>true).each {|gallery| gallery.update_attribute(:visibility,'public') }
+  	 Gallery.where(:public=>false).each {|gallery| gallery.update_attribute(:visibility,'public') }
   	 remove_column :galleries, :public
   end
 end
