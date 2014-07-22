6c74945ebc14560bf40d8823f52055c2efb4da83
Peter Mangiafico <peter@mangiafico.org>
2014-07-22 15:19:51 -0700

    fix bug with sorting of saved items/favorites and gallery highlights

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,8 +4,10 @@ class Gallery < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, :use => [:slugged]
 
+  scope :public_galleries, where(:visibility=>'public',:gallery_type=>'user')
+
   include RankedModel
-  ranks :row_order,:column => :position
+  ranks :row_order,:column => :position, :scope=>:public_galleries
 
   GALLERY_TYPES=%w{favorites user}
   VISIBILITY_TYPES=%w{public private curator}
@@ -22,9 +24,17 @@ class Gallery < ActiveRecord::Base
   validate :only_one_favorites_list_per_user
 
   def self.featured
-    self.where(:featured=>true,:visibility=>'public').order('position ASC,created_at DESC')
+    self.public_galleries.where(:featured=>true).where('saved_items_count > 0').rank(:row_order)
   end
   
+  def self.curated
+    self.public_galleries.where('saved_items_count > 0').includes(:user).where("users.role in ('curator','admin')").rank(:row_order)
+  end
+
+  def self.regular_users
+    self.public_galleries.where('saved_items_count > 0').includes(:user).where("users.role = 'user'").rank(:row_order)
+  end
+    
   def public
     visibility.to_sym == :public
   end
@@ -36,7 +46,7 @@ class Gallery < ActiveRecord::Base
 
   # custom has_many association, so we can add visibility filtering -- get the galleries items, pass in a second user (like the logged in user) to decide if hidden items should be returned as well
   def saved_items(user=nil)
-    User.visibility_filter(SavedItem.where(:gallery_id=>id),'saved_items',user).order("position ASC")
+    User.visibility_filter(SavedItem.where(:gallery_id=>id),'saved_items',user).rank(:row_order)
   end
 
   def only_one_favorites_list_per_user
