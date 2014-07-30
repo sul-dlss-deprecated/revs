428c8a59c5e6ed9dbdfc228ec8e8c097155d08e1
Peter Mangiafico <peter@mangiafico.org>
2014-07-29 17:08:44 -0700

    some more changes and logging of output for sitemap creation

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -35,16 +35,27 @@ SitemapGenerator::Sitemap.create do
   add contact_us_path, :changefreq => 'monthly'
   add tutorials_path, :changefreq => 'monthly'
 
+  collections=SolrDocument.all_collections
+
+  puts "Found #{collections.size} total collections"
+  total_images=0
+
   # iterate over all collections and all add all items
-  SolrDocument.all_collections.each do |collection|
+  collections.each do |collection|
 
       add collection_path(collection.id), :lastmod => collection['timestamp'] # add the collection object
 
-      collection.get_members(:include_hidden=>false, :rows=> @max_expected_collection_size).each do |doc|
-        add item_path(doc.id), :lastmod => doc['timestamp']
+      result=Blacklight.solr.select(:params => {:q=>'is_member_of_ssim:"' + collection.id + '"',:rows=>@max_expected_collection_size}) # get all members of the collection
+      puts ".working on collection #{collection.id}: #{collection.title} with #{result['response']['numFound']} total images"
+      total_images+= result['response']['numFound']
+
+      result['response']['docs'].each do |doc|
+        add item_path(doc['id']), :lastmod => doc['timestamp']
       end
 
   end
 
+  puts ""
+  puts "Found #{total_images} total images in #{collections.size} collections"
 
 end
