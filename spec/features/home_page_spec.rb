65d66c8bf9872028b4e293849b960ca8767b1d44
Gary Geisler <ggeisler@gmail.com>
2014-08-06 09:44:23 -0700

    Add new 'More to Explore' section to homepage

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,27 +1,14 @@
 require 'spec_helper'
 
 describe("Home Page",:type=>:request,:integration=>true) do
-  
-    it "should render the home page with correct number of total images" do
+
+    it "should render the home page with correct featured sections" do
       visit root_path
-      page.should have_content("Collection 1")
-      page.should have_content("The Revs Digital Library currently contains 16 items from 2 collections.")
-      login_as(user_login)
-      visit root_path
-      page.should have_content("The Revs Digital Library currently contains 16 items from 2 collections.")
+      page.should have_content I18n.t('revs.user_galleries.heading_featured')
+      page.should have_content I18n.t('revs.nav.more_galleries')
+      page.should have_content I18n.t('revs.nav.more_to_explore')
    end
 
-    it "should render the home page with correct number of total images when a curator or admin is logged in" do
-        logins=[curator_login,admin_login]
-        logins.each do |user|
-          login_as(user)
-          visit root_path
-          page.should have_content("Collection 1")
-          page.should have_content("The Revs Digital Library currently contains 17 items from 2 collections. This total includes 1 hidden items.")
-          logout
-        end
-    end
-    
     it "should give a nice error message page if we visit a bogus url" do
       visit "/bogusness"
       current_path.should == "/bogusness"
@@ -31,5 +18,5 @@ describe("Home Page",:type=>:request,:integration=>true) do
       page.should have_xpath("//a[contains(@href, \"/collection\")]")
       page.should have_xpath("//a[contains(@href, \"/contact\")]")
     end
-  
+
 end
