1e6801bd201196fb486df6d8161b5417f147d780
Peter Mangiafico <peter@mangiafico.org>
2014-11-18 14:05:08 -0800

    complete synonym support

--- db/schema.rb
+++ db/schema.rb
@@ -27,13 +27,12 @@ ActiveRecord::Schema.define(version: 20141027203015) do
 
 
   create_table "bookmarks", :force => true do |t|
-    t.integer  "user_id",       :null => false
+    t.integer  "user_id",     :null => false
     t.string   "document_id"
     t.string   "title"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "user_type"
-    t.string   "document_type"
   end
 
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
