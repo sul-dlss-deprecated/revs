ca4a81c64583416a418582978acbf61dbd833caa
Gary Geisler <ggeisler@gmail.com>
2015-04-17 12:15:37 -0700

    Update tests to reflect changes to Item Detail page
    Specifically, several tests expected to see the full title of an item on
    specific pages (e.g., user favorites page) on which we're now truncating long
    titles. Updates either change the expectation to the truncated title or use
    a different item for the test.

--- spec/features/favorites_spec.rb
+++ spec/features/favorites_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe("Favorites",:type=>:request,:integration=>true) do
 
   before :each do
-    logout 
+    logout
     @save_favorites_button=I18n.t('revs.favorites.save_to_favorites')
     @remove_favorites_button=I18n.t('revs.favorites.remove_from_favorites',:list_type=>'favorites')
     @druid1='qk978vx9753'
@@ -13,32 +13,32 @@ describe("Favorites",:type=>:request,:integration=>true) do
     @add_new_description=I18n.t('revs.favorites.you_can_add_a_note_html', :item_type=>'favorite',:href=>'add a note')
     @add_new_description_link= I18n.t('revs.favorites.you_can_add_a_note_linktext')
   end
-  
+
   it "should not show save/remove favorites link for non-logged in users" do
     visit catalog_path(@druid1)
     should_not_have_button(@save_favorites_button)
-    should_not_have_button(@remove_favorites_button)    
+    should_not_have_button(@remove_favorites_button)
   end
 
   it "should show save favorites link for a logged in user and allow them to add a favorite" do
-   
+
     user=get_user(user_login)
     login_as(user_login)
     visit catalog_path(@druid1)
     should_have_button(@save_favorites_button)
-    should_not_have_button(@remove_favorites_button)    
-    
+    should_not_have_button(@remove_favorites_button)
+
     # check database to be sure there are no favorites for this user
     expect(user.favorites.size).to eq(0)
 
     click_button(@save_favorites_button) # save the favorite
 
     should_not_have_button(@save_favorites_button) # button switches to remove
-    should_have_button(@remove_favorites_button)    
+    should_have_button(@remove_favorites_button)
 
     expect(user.favorites.where(:druid=>@druid1).size).to eq(1) # favorite is now saved
     expect(user.favorites.reload.size).to eq(1) # user now has one favorite
-        
+
     # druid2 is not a favorite yet
     visit catalog_path(@druid2)
     should_have_button(@save_favorites_button)
@@ -51,16 +51,15 @@ describe("Favorites",:type=>:request,:integration=>true) do
     click_button(@remove_favorites_button) # get rid of the favorite
 
     should_have_button(@save_favorites_button) # button switches back to add
-    should_not_have_button(@remove_favorites_button)    
-    
+    should_not_have_button(@remove_favorites_button)
+
     # favorite is gone
     expect(user.favorites.where(:druid=>@druid2).size).to eq(0) # favorite is now gone
-    expect(user.favorites.reload.size).to eq(1) # user now has one favorite    
-    
+    expect(user.favorites.reload.size).to eq(1) # user now has one favorite
   end
-  
+
   it "should not show any favorites on a user's profile page if they don't have any favorites" do
-    
+
     login_as(user_login)
     visit user_path(user_login)
     expect(page).to have_content I18n.t('revs.favorites.you_can_save_favorites')
@@ -78,10 +77,10 @@ describe("Favorites",:type=>:request,:integration=>true) do
     visit user_favorites_user_index_path(curator_login)
     expect(page).to have_content "Bryar 250 Trans-American:10" # this one is hidden, but shows up for curators
     expect(page).to have_content "A Somewhat Shorter Than Average Title"
-    expect(page).to have_content "Thompson Raceway, May 1 -- AND A long title can go here so let's see how it looks when it is really long"
+    expect(page).to have_content "Thompson Raceway, May 1 -- AND A long title can..."
     expect(page).to have_content "Marlboro 12 Hour, August 12-14"
   end
- 
+
    it "should not show hidden favorites to a non-logged in user on a public user's profile page if they are hidden" do
     visit user_path(curator_login)
     expect(page).not_to have_content I18n.t('revs.favorites.you_can_save_favorites')
@@ -89,7 +88,7 @@ describe("Favorites",:type=>:request,:integration=>true) do
     visit user_favorites_user_index_path(curator_login)
     expect(page).not_to have_content "Bryar 250 Trans-American:10" # this one is hidden, and does not show up for non logged in users
     expect(page).to have_content "A Somewhat Shorter Than Average Title"
-    expect(page).to have_content "Thompson Raceway, May 1 -- AND A long title can go here so let's see how it looks when it is really long"
+    expect(page).to have_content "Thompson Raceway, May 1 -- AND A long title can..."
     expect(page).to have_content "Marlboro 12 Hour, August 12-14"
   end
 
@@ -104,20 +103,20 @@ describe("Favorites",:type=>:request,:integration=>true) do
   end
 
   it "should allow a user to see a list of his favorites that paginates and allow the user to move between pages" do
-    
+
     original_default_per_page=Revs::Application.config.num_default_per_page
     Revs::Application.config.num_default_per_page=5 # make it a smaller number to facilitate paging test
 
     description = "This is a decription "
     login_as(user_login)
-    
+
     #Get a two page list of druids
     fav_druids = (item_druids-default_hidden_druids).first(Revs::Application.config.num_default_per_page*2)
-    fav_druids.each do |druid| 
+    fav_druids.each do |druid|
       visit catalog_path(druid)
-      click_button(@save_favorites_button) 
+      click_button(@save_favorites_button)
     end
-    #Check Out Pagination 
+    #Check Out Pagination
     visit user_favorites_user_index_path(user_login)
     expect(page).to have_content @Next
     expect(page).not_to have_content @Previous
@@ -130,16 +129,16 @@ describe("Favorites",:type=>:request,:integration=>true) do
 
     Revs::Application.config.num_default_per_page=original_default_per_page
   end
-  
+
 
   it "should allow a user to edit a favorite when viewing all their favorites" do
       new_description="Hello, I am a new description for a favorite."
-      
+
       login_as(user_login)
-      visit catalog_path(@druid1)
+      visit catalog_path(@druid2)
       click_button(@save_favorites_button)
       visit user_favorites_user_index_path(user_login)
-      expect(page).to have_content get_title_from_druid(@druid1)
+      expect(page).to have_content get_title_from_druid(@druid2)
       expect(page).to have_content @add_new_description
       expect(page).to have_no_content new_description
       click_link(@add_new_description_link)
@@ -147,40 +146,39 @@ describe("Favorites",:type=>:request,:integration=>true) do
       fill_in('saved_item_description', :with => new_description)
       click_button(I18n.t('revs.nav.update'))
       expect(page).to have_content I18n.t('revs.favorites.edit_item_note')
-      
+
       #Go check databse to make sure this was updated
       user = get_user(user_login)
       expect(user.favorites[0].description).to eq(new_description)
-      
+
       #Go back to the favorites page and ensure the new description is still there
       logout
       login_as(user_login)
       visit user_favorites_user_index_path(user_login)
       expect(page).to have_content new_description
-      expect(page).to have_content get_title_from_druid(@druid1)
-      
+      expect(page).to have_content get_title_from_druid(@druid2)
   end
-  
+
   it "should allow a user to remove a favorite when viewing all their favorites" do
     login_as(user_login)
     user = get_user(user_login)
     expect(user.favorites.size).to eq(0)
-    visit catalog_path(@druid1)
+    visit catalog_path(@druid2)
     click_button(@save_favorites_button)
     user = get_user(user_login)
     expect(user.favorites.size).to eq(1)
     visit user_favorites_user_index_path(user_login)
-    expect(page).to have_content get_title_from_druid(@druid1)
+    expect(page).to have_content get_title_from_druid(@druid2)
     click_button(@remove_favorites_button)
     user = get_user(user_login)
     expect(user.favorites.size).to eq(0)
     logout
     login_as(user_login)
     visit user_favorites_user_index_path(user_login)
-    expect(page).to have_no_content get_title_from_druid(@druid1)
+    expect(page).to have_no_content get_title_from_druid(@druid2)
     expect(page).to have_content I18n.t('revs.favorites.none')
   end
- 
+
    it "should not allow a curator to add a hidden item to their favorites or gallery" do
     hidden_druid=default_hidden_druids.first
     login_as(curator_login)
@@ -188,7 +186,7 @@ describe("Favorites",:type=>:request,:integration=>true) do
     expect(page).to have_no_content @save_favorites_button
     expect(page).to have_no_content I18n.t('revs.user_galleries.add_to_gallery')
     should_not_have_button("Add") # add to gallery button
-  end 
+  end
 
   it "should automatically create the default favorites list if it does not exist for an existing user when they login" do
     beta_user=get_user(beta_login)
@@ -198,5 +196,5 @@ describe("Favorites",:type=>:request,:integration=>true) do
     expect(beta_user.favorites_list).not_to be_nil # created it
     expect(beta_user.favorites_list.class).to eq(Gallery)
   end
-  
+
 end
--- spec/features/item_page_spec.rb
+++ spec/features/item_page_spec.rb
@@ -6,19 +6,19 @@ describe("Item Pages",:type=>:request,:integration=>true) do
     visit catalog_path('jg267fg4283')
     expect(find('.show-document-title')).to have_content('Untitled')
   end
-  
-  it "should show an item detail page with image and linked facet metadata with no description" do 
+
+  it "should show an item detail page with image and linked facet metadata with no description" do
     visit catalog_path('yt907db4998')
     expect(find('.show-document-title')).to have_content('Record 1')
     expect(page).to have_content('2011-023DUG-3.0_0017')
     expect(page).to have_content('slides')
     expect(page).to have_content('1960')
     expect(page).to have_content('John Dugdale Collection')
-    expect(page).not_to have_content('Description:')    
+    expect(page).not_to have_content('Description')
     expect(page).to have_xpath("//img[contains(@src, \"image/yt907db4998/2011-023DUG-3.0_0017_thumb\")]")
-    expect(page).to have_xpath("//a[contains(@href, \"/catalog?f%5Bformat_ssim%5D%5B%5D=slides\")]")    
-    expect(page).to have_xpath("//a[contains(@href, \"/catalog?f%5Bcollection_ssim%5D%5B%5D=John+Dugdale+Collection\")]")    
-    expect(page).to have_xpath("//a[contains(@href, \"/catalog?range%5Bpub_year_isim%5D%5Bbegin%5D=1960&range%5Bpub_year_isim%5D%5Bend%5D=1960\")]")    
+    expect(page).to have_xpath("//a[contains(@href, \"/catalog?f%5Bformat_ssim%5D%5B%5D=slides\")]")
+    expect(page).to have_xpath("//a[contains(@href, \"/catalog?f%5Bcollection_ssim%5D%5B%5D=John+Dugdale+Collection\")]")
+    expect(page).to have_xpath("//a[contains(@href, \"/catalog?range%5Bpub_year_isim%5D%5Bbegin%5D=1960&range%5Bpub_year_isim%5D%5Bend%5D=1960\")]")
     expect(page).to have_content("John Dugdale Collection (2)")
     expect(page).to have_content("View all collection items")
   end
@@ -28,19 +28,19 @@ describe("Item Pages",:type=>:request,:integration=>true) do
     expect(find('.show-document-title')).to have_content('Marlboro 12 Hour, August 12-14')
   end
 
-  it "should show an item detail page that has a description" do 
+  it "should show an item detail page that has a description" do
     visit catalog_path('yh093pt9555')
     expect(find('.show-document-title')).to have_content('Marlboro 12 Hour, August 12-14')
-    expect(page).to have_content('Description:')    
+    expect(page).to have_content('Description')
     expect(page).to have_content('This is the description of this image')
-    expect(page).to have_content('Year:')
+    expect(page).to have_content('Year')
     expect(page).to have_content('1955')
-    expect(page).to have_content('Collection:')
+    expect(page).to have_content('Collection')
     expect(page).to have_content('David Nadig Collection')
     expect(page).to have_xpath("//img[contains(@src, \"image/yh093pt9555/2012-027NADI-1966-b1_6.4_0011\")]")
   end
 
-  it "should show a 404 error message when you visit an invalid ID" do 
+  it "should show a 404 error message when you visit an invalid ID" do
     visit catalog_path('yh093pt9554')
     expect(current_path).to eq(catalog_path('yh093pt9554'))
     expect(page).to have_content(I18n.t('revs.errors.404_message'))
@@ -51,20 +51,20 @@ describe("Item Pages",:type=>:request,:integration=>true) do
     visit catalog_path('yh093pt9555') # Item with Vehicle and Race field values
     expect(find('.show-document-title')).to have_content('Marlboro 12 Hour, August 12-14')
     expect(page).to have_content('Vehicle Information')
-    expect(page).to have_content('Marque:')
+    expect(page).to have_content('Marque')
     expect(page).to have_content('Ford, Chevy')
-    expect(page).to have_content('Model:')
+    expect(page).to have_content('Model')
     expect(page).to have_content('Camaro, Mustang, Camaro')
-    expect(page).to have_content('Model Year:')
+    expect(page).to have_content('Model Year')
     expect(page).to have_content('1950, 1951')
-    expect(page).to have_content('Vehicle Markings:')
+    expect(page).to have_content('Vehicle Markings')
     expect(page).to have_content('Oil on hood')
     expect(page).to have_content('Race Information')
-    expect(page).to have_content('Event:')
+    expect(page).to have_content('Event')
     expect(page).to have_content('Indy Race')
-    expect(page).to have_content('Venue:')
+    expect(page).to have_content('Venue')
     expect(page).to have_content('Indy 500 Speedway')
-    expect(page).to have_content('Race Data:')
+    expect(page).to have_content('Race Data')
     expect(page).to have_content('This is who won, who lost, other bits about the race.')
     visit catalog_path('xf058ys1313') # Item without Vehicle and Race field values
     expect(page).to have_content('Thompson Raceway, May 2')
