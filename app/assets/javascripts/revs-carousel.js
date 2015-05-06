7fdd5861c7be47c3475f15e60dcb7cc7e75b9beb
Gary Geisler <ggeisler@gmail.com>
2015-05-06 10:57:22 -0700

    Update carousel on collection detail page

--- app/assets/javascripts/revs-carousel.js
+++ app/assets/javascripts/revs-carousel.js
@@ -1,27 +1,15 @@
 $(document).ready(function(){
-	// Carousel on Collection show page
-  $("#collection_carousel").bind('slid', function(){
-	  var carousel = $(this);
-	  var druid= $('.carousel-inner', carousel).attr('druid');
-	  var index = parseInt($('.active', carousel).attr('image-number'));
-	  var rows = parseInt($('.carousel-inner', carousel).attr('rows'));
-	  var start = parseInt($('.carousel-inner', carousel).attr('start'));
-	  var total = parseInt($('.carousel-inner', carousel).attr('total'));
-	  var end=parseInt(start)+parseInt(rows)
-	  $(".iterator", carousel).text(index);
-		if (index == 1) { $('#back-carousel-button').hide();}
-			else
-			{$('#back-carousel-button').show();}// enable back button unless we are on the first image
-		if (index == total) { $('#forward-carousel-button').hide();}
-			else
-			{$('#forward-carousel-button').show();}// enable forward button unless we are on the last image
-	  if (index == end - 2) { // fetch more items when we get close to the end of our set
-		  $.getScript("/update_carousel?druid=" + druid + "&rows=" + rows + "&start=" + end)
-		}
-	});
 
-  // Core jCarousel
+  // Initialize homepage carousel
   $('.jcarousel').jcarousel();
+
+  // Initialize collection details carousel with autoscrolling at 3 second interval
+  $('.collection_carousel').jcarousel()
+      .jcarouselAutoscroll({
+        interval: 3000,
+        target: '+=1',
+        autostart: true
+      });
 
   $('.jcarousel-control-prev')
       .on('jcarouselcontrol:active', function() {
@@ -46,12 +34,38 @@ $(document).ready(function(){
       });
 
   // Carousel on home page
+  // When new item slides into view, update metadata shown for item
   $('.jcarousel').on('jcarousel:targetin', 'li', function() {
     $("#featured-collection-nav #collection-title-link").text($(this).attr("data-collection-title"));
     $("#featured-collection-nav #collection-title-link").attr('href', $(this).attr("data-collection-href"));
     $("#featured-collection-nav #collection-image-link").attr('href', $(this).attr("data-collection-href"));
     $("#featured-collection-nav #collection-description").text($(this).attr("data-collection-description"));
     $("#featured-collection-nav #collection-link").attr('href', $(this).attr("data-collection-href"));
+  });
+
+  // Carousel on collection details page
+  // When new item slides into view, update caption and variables used to get more items
+  $('.collection_carousel').on('jcarousel:targetin', 'li', function() {
+    var current_item = $(this);
+    current_item.addClass('active');
+    var druid= $('.collection_carousel').attr('data-druid');
+    var index = parseInt(current_item.attr('data-image-number'));
+    var rows = parseInt($('.collection_carousel').attr('rows'));
+    var start = parseInt($('.collection_carousel').attr('start'));
+    var total = parseInt($('.collection_carousel').attr('total'));
+    var end = parseInt(start) + parseInt(rows);
+    $(".iterator", '.jcarousel-wrapper').text(index);
+    if (index == end - 2) { // fetch more items when we get close to the end of our set
+      $.getScript("/update_carousel?druid=" + druid + "&rows=" + rows + "&start=" + end);
+    }
+    // Ensure jCarousel knows about remotely added items and makes next button active
+    if ((index % 5) == 0) {
+      $('.collection_carousel').jcarousel('reload');
+    }
+  });
+
+  $('.collection_carousel').on('jcarousel:targetout', 'li', function() {
+    $(this).removeClass('active');
   });
 
 });
