c4ace9c39f38d0e41caa1f60f5125cdae1017220
Gary Geisler <ggeisler@gmail.com>
2015-06-03 12:20:29 -0700

    Fix references to images in curator help pages
    Needed to update image references because image links were broken when
    deploying to server (related to moving to Rails 4 asset pipeline).
    
    To do this required moving help page text from locale files to view files,
    so we could use Rails tags to reference image assets.

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Revs
 
     # Custom i18n.load_path to pick up locale files in subdirectories
     config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
-        
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
@@ -61,28 +61,34 @@ module Revs
     # config.active_record.schema_format = :sql
 
     # Enable the asset pipeline
-    config.assets.enabled = true    
+    config.assets.enabled = true
+
+    # Make sure all image subdirectories are added to assets paths
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+      config.assets.paths << path
+    end
+
     # Default SASS Configuration, check out https://github.com/rails/sass-rails for details
     config.assets.compress = !Rails.env.development?
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = VERSION
 
-    config.version = VERSION # read from VERSION file at base of website                                                                                                                                                                                                                                                   
+    config.version = VERSION # read from VERSION file at base of website
 
     # Revs App Specific Configuration
-    config.stacks_url = YAML.load_file("#{Rails.root}/config/stacks.yml")[Rails.env]["url"]                                                                                                                                                                                                                                 
+    config.stacks_url = YAML.load_file("#{Rails.root}/config/stacks.yml")[Rails.env]["url"]
     config.contact_us_topics = {'default'=>'revs.contact.select_topic', 'metadata'=>'revs.contact.metadata_issue','terms of use'=>'revs.contact.terms_of_use', 'error'=>'revs.contact.problem','other'=>'revs.contact.other_questions'} # sets the list of topics shown in the contact us page
     config.contact_us_recipients = {'default'=>'digcoll@jirasul.stanford.edu', 'terms of use'=>'ldrake@chmotorcars.com','metadata'=>'digcoll@jirasul.stanford.edu','error'=>'digcoll@jirasul.stanford.edu','other'=>'digcoll@jirasul.stanford.edu'} # sets the email address for each contact us topic configed aboveend
     config.contact_us_cc_recipients = {'default'=>'revs-other@jirasul.stanford.edu', 'metadata'=>'revs-metadata-comment@jirasul.stanford.edu', 'error'=>'revs-problems@jirasul.stanford.edu','other'=>'revs-other@jirasul.stanford.edu'} # sets the CC email address for each contact us topic configed above
 
     config.collections_not_available_for_reproduction = ['td221fy0182','jh550nq3200','kv107xd8164'] # these collections are not available for reproduction and will show a special statement instead of the use and reproduction statement in the item itself...currently Breslauer, Bochroch and Worner
-    
+
     config.revs_reuse_link='http://revsinstitute.org/research-education/permission-to-use/'
-    
+
     config.collier_archive_name = 'Collier Collection' # this is the name of the collier archive, it will be added to records if it does not yet exist when saving
-    
-    config.num_latest_user_activity = 3 # the latest number of flags/annotations to show on the user profile page     
+
+    config.num_latest_user_activity = 3 # the latest number of flags/annotations to show on the user profile page
     config.num_flags_per_item_per_user = 5 # the number of times each user is allowed to flag a particular item
     #config.flag_sort_display = {FLAG_STATES[:open]=> I18n.t('revs.flags.open_state_display_name'),FLAG_STATES[:fixed]=> I18n.t('revs.flags.fixed_state_display_name'),FLAG_STATES[:wont_fix]=> I18n.t('revs.flags.wont_fix_state_display_name')}
     config.num_default_per_page = 25 # the default number of a given item to display per page (e.g. flags, favorites, galleries)
