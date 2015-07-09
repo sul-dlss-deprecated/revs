755e67f0c51893532e0618759f90974a7f7a3070
Gary Geisler <ggeisler@gmail.com>
2015-07-09 12:25:53 -0700

    Update pagination so we always show links to first and last pages

---
+++ config/initializers/kaminari_config.rb
@@ -0,0 +1,10 @@
+Kaminari.configure do |config|
+  # config.default_per_page = 10
+  # config.max_per_page = nil
+  config.window = 2
+  config.outer_window = 2
+  # config.left = 0
+  # config.right = 2
+  # config.page_method_name = :page
+  # config.param_name = :page
+end
