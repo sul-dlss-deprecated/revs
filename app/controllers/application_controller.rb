7db30001e135623e4f0a3ba0c6fdb96b9ec2360d
Peter Mangiafico <peter@mangiafico.org>
2015-10-28 15:20:53 -0700

    whitelist sort ordering to avoid sql injection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,34 @@
 # encoding: utf-8
-require 'squash/rails' 
+require 'squash/rails'
 require 'addressable/uri'
 
 class ApplicationController < ActionController::Base
-  # Adds a few additional behaviors into the application controller 
-  
+  # Adds a few additional behaviors into the application controller
+
    include Blacklight::Controller
    include DateHelper
    extend DateHelper
    include SolrQueryHelper
-   
+
    require 'blacklight/catalog/search_context'
    include Blacklight::Catalog::SearchContext
 
    # include squash.io
    include Squash::Ruby::ControllerMethods
-    
+
   rescue_from Exception, :with=>:exception_on_website
-      
-  # Please be sure to impelement current_user and user_session. Blacklight depends on 
-  # these methods in order to perform user specific actions. 
+
+  # Please be sure to impelement current_user and user_session. Blacklight depends on
+  # these methods in order to perform user specific actions.
 
   helper_method :application_name,:tag_line,:current_role,:on_home_page,:on_collections_page,:on_galleries_page,:on_about_pages,:on_detail_page,:show_terms_dialog?,:sunet_user_signed_in?,:in_search_result?,:list_type_interpolator,:item_type_interpolator
-  helper_method :paging_params,:extract_paging_params,:from_gallery?,:from_favorites?,:is_logged_in_user?
+  helper_method :paging_params,:extract_paging_params,:from_gallery?,:from_favorites?,:is_logged_in_user?,:sort_field,:whitelist_sort_fields
   layout "revs"
 
   protect_from_forgery
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   prepend_before_filter :simulate_sunet, :if=>lambda{Rails.env !='production' && !session["WEBAUTH_USER"].blank?} # to simulate sunet login in development, set a parameter in config/environments/ENVIRONMENT.rb
   before_filter :signin_sunet_user, :if=>lambda{sunet_user_signed_in? && !user_signed_in?} # signin a sunet user if they are webauthed but not yet logged into the site
 
@@ -37,15 +37,15 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     not_authorized(:additional_message=>exception.message)
   end
-  
+
   def application_name
     t('revs.digital_library')
   end
-  
+
   def tag_line
     t('revs.tagline')
   end
- 
+
   def repository_counts
     @total_collections=SolrDocument.all_collections.size
     @total_images=SolrDocument.total_images
@@ -78,16 +78,16 @@ class ApplicationController < ActionController::Base
 
   def store_referred_page
     if [new_user_session_url,new_user_session_path,new_user_registration_url,new_user_registration_path,new_user_password_path,new_user_password_url,new_user_confirmation_path,new_user_confirmation_url,new_user_unlock_path,new_user_unlock_url,edit_user_password_path,edit_user_password_url].include?(strip_all_params(previous_page)) # referral pages cannot be sign in or sign up page
-      session[:login_redirect] = root_path 
+      session[:login_redirect] = root_path
     else
-      session[:login_redirect] = previous_page 
+      session[:login_redirect] = previous_page
     end
   end
-  
+
   def after_sign_in_path_for(resource)
     session[:login_redirect]  || root_path
   end
-     
+
   def redirect_home_if_signed_in # if the user is already signed in and they try to go to login/reg page, just send them to the home page to prevent infinite redirects
     if user_signed_in?
       session[:login_redirect] = nil
@@ -110,9 +110,9 @@ class ApplicationController < ActionController::Base
   end
 
   def no_sunet_users
-    not_authorized unless (user_signed_in? && !current_user.sunet_user?)     
+    not_authorized unless (user_signed_in? && !current_user.sunet_user?)
   end
-  
+
   def check_for_any_user_logged_in
     not_authorized unless user_signed_in?
   end
@@ -120,7 +120,7 @@ class ApplicationController < ActionController::Base
   def check_for_user_logged_in
     not_authorized unless user_signed_in? && current_user.role?(:user)
   end
-  
+
   def check_for_admin_logged_in
     not_authorized unless can? :administer, :all
   end
@@ -128,24 +128,24 @@ class ApplicationController < ActionController::Base
   def check_for_curator_logged_in
     not_authorized unless can? :curate, :all
   end
-  
+
   def in_search_result?
     @previous_document || @next_document
   end
-      
+
   def ajax_only
     unless request.xhr?
       render :nothing=>true, :status => 405
       return
     end
   end
-  
+
   def set_no_cache
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
- 
+
   def routing_error
     # flash.now[:error]=t('revs.routing_error')
     render "application/404", :formats=>[:html], :status => :not_found
@@ -153,13 +153,13 @@ class ApplicationController < ActionController::Base
   end
 
   def not_authorized(params={})
-    
+
     additional_message=params[:additional_message]
     replace_message=params[:replace_message]
-    
+
     message = replace_message || t('revs.messages.not_authorized')
     message+=" " + additional_message unless additional_message.blank?
-    
+
     respond_to do |format|
       format.html { redirect_to :root, :alert=>message.html_safe}
       format.xml  { render :xml => message, :status=>401 }
@@ -173,18 +173,18 @@ class ApplicationController < ActionController::Base
   def simulate_sunet
     request.env["WEBAUTH_USER"]=session["WEBAUTH_USER"] unless Rails.env=='production'
   end
-  
+
   def signin_sunet_user
      # if we have a webauthed user who is not yet signed in, let's sign them in or create them a new user account if needed
-    user=(User.where(:sunet=>request.env["WEBAUTH_USER"]).first || User.create_new_sunet_user(request.env["WEBAUTH_USER"])) 
-    sign_in user unless request.path==user_session_path    
+    user=(User.where(:sunet=>request.env["WEBAUTH_USER"]).first || User.create_new_sunet_user(request.env["WEBAUTH_USER"]))
+    sign_in user unless request.path==user_session_path
     user.increment!(:login_count)
   end
-  
+
   def sunet_user_signed_in?
     !request.env["WEBAUTH_USER"].blank?
   end
-    
+
   def on_home_page
     request.path==root_path && params[:f].blank? && params[:q].blank? && params[:range].blank?
   end
@@ -192,7 +192,7 @@ class ApplicationController < ActionController::Base
   def on_detail_page
     controller_path=='catalog' && action_name=='show'
   end
-  
+
   def on_collections_page
     controller_path=='collection'
   end
@@ -208,7 +208,7 @@ class ApplicationController < ActionController::Base
   def seen_terms_dialog?
     cookies[:seen_terms] || false
   end
-  
+
   def show_terms_dialog?
    false # do not show terms dialog as per email with Trina Purcell on August 28, 2014
    # %w{production staging}.include?(Rails.env) && !seen_terms_dialog?   # we are using the terms dialog to show a warning to users who are viewing the site on production or staging
@@ -218,7 +218,7 @@ class ApplicationController < ActionController::Base
     cookies[:seen_terms] = { :value => true, :expires => 7.days.from_now } # they've seen it now, don't show it for another week
     redirect_to (params[:return_to] || :root)
   end
- 
+
   def list_type_interpolator(gallery_type)
     (gallery_type == 'favorites' ? t('revs.favorites.plural') : t('revs.user_galleries.singular'))
   end
@@ -230,11 +230,11 @@ class ApplicationController < ActionController::Base
   def current_role
     current_user ? current_user.role : 'none'
   end
-  
+
   def current_ability
     current_user ? current_user.ability : User.new.ability
   end
-  
+
   # add paging params to an incoming hash of other parameters
   def paging_params(others={})
     others.merge({:order=>@order,:per_page=>@per_page,:page=>@current_page})
@@ -243,10 +243,47 @@ class ApplicationController < ActionController::Base
   # get the current paging params and set instance variables
   def get_paging_params
    @current_page = params[:page] || 1
-   @order=params[:order] || 'created_at DESC'
+   @order=sort_field(params[:order])
    @per_page=(params[:per_page] || Revs::Application.config.num_default_per_page).to_i
    @from=params[:from]
    @search=params[:search]
+  end
+
+  # pass all querystring sort parameters through a whitelist and set a default if no valid field was found
+  def sort_field(sort_param,default='created_at DESC')
+    whitelist_sort_fields[sort_param] || default
+  end
+
+  # the allowed sort orders that can be passed in via the querystring, map these to actual values to pass to SQL to prevent sql injection
+  def whitelist_sort_fields
+    {
+      'user_id' => 'user_id ASC',
+      'email' => 'email ASC',
+      'title' => 'title ASC',
+      'items_title' => 'items.title ASC',
+      'visibility' => 'visibility ASC',
+      'views' => 'views DESC',
+      'username' => 'username ASC',
+      'last_name' => 'last_name ASC',
+      'sunet' => 'sunet ASC',
+      'role' => 'role ASC',
+      'login_count_desc' => 'login_count DESC',
+      'created_at_desc' => 'created_at DESC',
+      'updated_at_desc' => 'updated_at DESC',
+      'confirmed_at_desc' => 'confirmed_at DESC',
+      'num_flags_desc' => 'num_flags DESC',
+      'flags_created_at_desc' => 'flags.created_at DESC',
+      'flags_updated_at_desc' => 'flags.updated_at DESC',
+      'annotations_created_at_desc' => 'annotations.created_at DESC',
+      'annotations_updated_at_desc' => 'annotations.updated_at DESC',
+      'num_annotations_desc' => 'num_annotations DESC',
+      'num_favorites_desc' => 'num_favorites DESC',
+      'num_galleries_desc' => 'num_galleries DESC',
+      'items_title_asc' => 'items.title ASC',
+      'num_edits_desc' => 'num_edits DESC',
+      'saved_items_updated_at_desc' => 'saved_items.updated_at DESC',
+      'galleries_saved_items_count_desc' => 'galleries.saved_items_count DESC',
+    }
   end
 
   # extract relevant paging params from params hash to add to some links
@@ -264,7 +301,7 @@ class ApplicationController < ActionController::Base
 
 
   def exception_on_website(exception)
-   
+
     @exception=exception
     notify_squash exception
 
@@ -277,11 +314,11 @@ class ApplicationController < ActionController::Base
         raise(@exception)
      end
   end
-  
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:password,:password_confirmation,:username, :email,:subscribe_to_revs_mailing_list,:subscribe_to_mailing_list) }
   end
-      
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,15 @@
 class UserController < ApplicationController
-    
+
   before_filter :load_user_profile, :except=>[:update_flag_table,:curator_update_flag_table] # we need to be sure the profile exists before doing anything
   before_filter :get_paging_params, :only=>[:annotations,:favorites,:galleries,:edits,:flags]
-  
+
   authorize_resource
-  
+
   # user profile pages
-  
+
   def show
     @page_title=I18n.t('revs.user.user_dashboard',:name=>(is_logged_in_user?(@user) ? t('revs.user.your') : "#{@user.to_s}'s"))
-  end    
+  end
 
   # all of the user's annotations
   def annotations
@@ -22,44 +22,44 @@ class UserController < ApplicationController
     @manage=params[:manage]
     @view=params[:view] || "grid"
     unsorted_favorites = @user.favorites(current_user)
-    
+
     #If the user has just been deleted favorites, @current_page might exceed the number of favorites
     max_pages = unsorted_favorites.count / @per_page
-    
+
     #Add in one extra page for any overflows over the current amount
-    max_pages += 1 if unsorted_favorites.count % @per_page != 0   
+    max_pages += 1 if unsorted_favorites.count % @per_page != 0
 
     #Reset @current_page if needed
     @current_page = max_pages.to_s if @current_page.to_i > max_pages
-    
+
     @saved_items=Kaminari.paginate_array(unsorted_favorites.order(@order)).page(@current_page).per(@per_page)
     @page_title="#{@user.to_s}: #{I18n.t('revs.favorites.head')}"
-    
+
   end
-  
+
  def galleries
    @galleries=@user.galleries(current_user).page(@current_page).per(@per_page)
    @page_title="#{@user.to_s}: #{I18n.t('revs.nav.galleries')}"
  end
- 
+
   # all of the user's item edits
   def edits
-    @order="change_logs.#{@order}" if @order.downcase == 'created_at desc'
+    @order="change_logs.#{@order}" if @order == 'created_at desc'
     @edits=@user.metadata_updates.order(@order).page(@current_page).per(@per_page)
     @page_title="#{@user.to_s}: #{I18n.t('revs.curator.edits')}"
   end
-  
+
   # all of the user's flags
   def flags
     s = params[:selection] || Flag.open
-    @selection = s.split(',')    
-    @flags=flagListForStates(@selection, @user, @order)      
+    @selection = s.split(',')
+    @flags=flagListForStates(@selection, @user, @order)
     @all_flag_count=@user.flags.count
     @page_title="#{@user.to_s}: #{I18n.t('revs.flags.plural')}"
   end
-  
+
   def update_flag_table
-    @curate_view = false 
+    @curate_view = false
     @username=params[:username]
     @selection = params[:selection].split(',')
     @user = User.find(@username)
@@ -68,39 +68,39 @@ class UserController < ApplicationController
        format.js { render }
     end
   end
-  
+
   def curator_update_flag_table
-    @curate_view = true 
-    @selection = params[:selection].split(',') 
+    @curate_view = true
+    @selection = params[:selection].split(',')
     @flags = flagListForStates(@selection, nil,params[:sort] || "druid")
     respond_to do |format|
        format.js { render }
     end
   end
 
-  
+
   def flagListForStates(states, user, sort)
     flags = []
-      
+
       if user == nil #curator, we want all flags
          temp = Flag.where(:state=>states).includes(:item).order(sort)
       else
         temp = user.flags(current_user).includes(:item).where(:state=>states, :user_id=> @user.id).order(sort)
       end
-     
-    flags = temp || []  
-      
+
+    flags = temp || []
+
     return Kaminari.paginate_array(flags).page(@current_page).per(Revs::Application.config.num_default_per_page)
   end
-  
-  private  
+
+  private
   def load_user_profile
     begin
       @user = User.find(params[:id])
     rescue
       profile_not_found
       return
-    end      
+    end
     @user.create_default_favorites_list # create the default favorites list if for some reason it does not exist
     @latest_annotations=@user.annotations(current_user).order('created_at desc').limit(Revs::Application.config.num_latest_user_activity)
     @latest_flags=@user.flags(current_user).where(:state=>Flag.open).order('created_at desc').limit(Revs::Application.config.num_latest_user_activity)
@@ -108,10 +108,10 @@ class UserController < ApplicationController
     @latest_galleries=@user.galleries(current_user).order('created_at desc').limit(Revs::Application.config.num_latest_user_activity)
     @latest_favorites=@user.favorites(current_user).order('saved_items.created_at desc').limit(Revs::Application.config.num_latest_user_activity)
   end
-  
+
   def profile_not_found
     flash[:error]=t('revs.authentication.user_not_found')
-    redirect_to root_path 
+    redirect_to root_path
   end
-  
+
 end
