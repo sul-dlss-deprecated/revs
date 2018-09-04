165caf5b067a2817370e6a7d300faadbde720e91
Peter Mangiafico <peter@mangiafico.org>
2018-09-04 12:17:01 -0700

    update env and session variables for webauth to shib change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 # this overrides the Devise Session Controller, so we can do some specific things at login time
 class SessionsController < Devise::SessionsController
-  
+
   # sign in form
   def new
     return if redirect_home_if_signed_in
     store_referred_page
     super
   end
-  
+
   # sign in form submit method
   def create
     user=User.where('email=? or username=?',params[:user][:login],params[:user][:login])
@@ -19,7 +19,7 @@ class SessionsController < Devise::SessionsController
       super
     end
   end
-  
+
   def destroy
     session[:curator_edit_mode] = nil
     super
@@ -27,18 +27,18 @@ class SessionsController < Devise::SessionsController
 
   def webauth_login
     if Revs::Application.config.simulate_sunet_user && Rails.env != 'production' # if we are simulating sunet logins and we are not in production, set a fake webauth cookie manually for testing
-      session["WEBAUTH_USER"]=Revs::Application.config.simulate_sunet_user
+      session["REMOTE_USER"]=Revs::Application.config.simulate_sunet_user
     end
     redirect_to params[:referrer] || root_url
   end
 
   def webauth_logout
     if Revs::Application.config.simulate_sunet_user && Rails.env != 'production' # if we are simulating sunet logins and we are not in production, kill the fake webauth cookie manually for testing
-     session["WEBAUTH_USER"]=nil
+     session["REMOTE_USER"]=nil
     end
     sign_out
-    flash[:notice] = t('revs.authentication.stanford_webauth_logout') unless request.env["WEBAUTH_USER"]
+    flash[:notice] = t('revs.authentication.stanford_webauth_logout') unless request.env["REMOTE_USER"]
     redirect_to root_url
   end
-    
+
 end
