1b7fb7ddc2e7ff7ea8de04a2a0d6775334411c69
Peter Mangiafico <peter@mangiafico.org>
2019-09-26 10:12:07 -0700

    disable new user registrations since the site is coming down soon

--- config/application.rb
+++ config/application.rb
@@ -90,7 +90,6 @@ module Revs
     config.contact_us_cc_recipients = {'default'=>'revs-other@jirasul.stanford.edu', 'metadata'=>'revs-metadata-comment@jirasul.stanford.edu', 'error'=>'revs-problems@jirasul.stanford.edu','other'=>'revs-other@jirasul.stanford.edu'} # sets the CC email address for each contact us topic configed above
 
     config.show_item_counts_in_header = false # if set to true, we will show total item and collection counts in the header
-    config.disable_editing = false # if set to true, will disallow metadata editing, changing visibility and placeholder, the creation of annotations and flags - the things that can update a solr document or add to the metadata editing load
     config.num_latest_user_activity = 3 # the latest number of flags/annotations to show on the user profile page
     config.num_flags_per_item_per_user = 5 # the number of times each user is allowed to flag a particular item
     #config.flag_sort_display = {FLAG_STATES[:open]=> I18n.t('revs.flags.open_state_display_name'),FLAG_STATES[:fixed]=> I18n.t('revs.flags.fixed_state_display_name'),FLAG_STATES[:wont_fix]=> I18n.t('revs.flags.wont_fix_state_display_name')}
@@ -110,7 +109,7 @@ module Revs
     #   {:question=>'What is the name of the car company that manufacturers the Mustang?',:answer=>'Ford'},
     #   {:question=>'What is the first name of the founder of Ferrari?',:answer=>'Enzo'}
     # ]
-    config.disable_new_registrations = false # set to true to disable new users from registering (useful in conjunction with disable_editing or if there is a sustained period of bogus registrations)
+    config.disable_new_registrations = true # set to true to disable new users from registering (useful in conjunction with disable_editing or if there is a sustained period of bogus registrations)
     config.disable_more_to_explore = true # set to true to disable more to explore on the home page
     config.disable_featured_galleries = true # set to true to disable featured galleries on the home page
     config.require_manual_account_activation = true # set to true to require an admin to manually activate any new account registrations
